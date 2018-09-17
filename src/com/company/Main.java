1a312e5a978e4c68bd28d9487b536bf80f596d09
jb838 <jacob.blasek1220@gmail.com>
2018-09-17 11:12:50 -0400

    winstons hair smells like air freshener

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("Type in your first word.");
+        String wordOne = kbReader.next();
+        System.out.println("Type in your second word.");
+        String wordTwo = kbReader.next();
+
+        if (wordOne.length() == wordTwo.length()) {
+            
+
+        }
+
+        else {
+            System.out.println("Your words are not anagrams of each other.");
+            System.exit(0);
+       }
+
+
     }
 }
