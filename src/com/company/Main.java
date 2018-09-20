6de619c91736fe8ca2ebbd00a88f75afe6ff0bde
jblasek <jacob.blasek1220@gmail.com>
2018-09-20 18:57:48 -0400

    A duck walked out to the lemonade stand and he said to the man running the stand, "Hey, bum bum bum, PUT THE MONEY IN THE BAG"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,42 @@
 package com.company;
 import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner kbReader = new Scanner(System.in);
+
         System.out.println("Type in your first word.");
         String wordOne = kbReader.next();
         System.out.println("Type in your second word.");
         String wordTwo = kbReader.next();
 
         if (wordOne.length() == wordTwo.length()) {
-            
+
+            char[] array1 = wordOne.toCharArray();
+            char[] array2 = wordTwo.toCharArray();
+
+
+            int count = 0;
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] == array2[i])
+                    count++;
+            }
+
+            if (count == array2.length) ;
+            System.out.println("Your words are anagrams of each other!");
 
         }
-
         else {
             System.out.println("Your words are not anagrams of each other.");
             System.exit(0);
-       }
+        }
+
+        }
+
+
 
 
     }
-}
+
